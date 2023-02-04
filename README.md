ffdccaa379d3d3f638a658c9c1e189a27a9723e2
Volodymyr Pivnychenko <pivnychenko@Volodymyrs-MacBook-Pro.local>
2023-02-04 13:15:22 -0800

    chore(project): project cascade

---
+++ README.md
@@ -0,0 +1,39 @@
+## Local run 
+
+
+
+```sh
+
+$ sudo chown -R $USER:$USER ~/Laravel-Vue-JS-Customer-data
+$ docker-compose build --no-cache
+$ docker-compose up 
+$ docker-compose exec app php artisan migrate 
+
+
+$ docker-compose exec app composer require laravel/ui
+$ docker-compose exec app php artisan route:list
+$ docker-compose exec app php artisan key:generate
+$ docker-compose exec app php artisan config:cache
+$ docker-compose exec app composer create-project laravel/laravel example-app
+$ docker-compose exec app php artisan make:controller <Name>
+$ docker-compose exec app php artisan make:model <Name>
+$ docker-compose exec app php artisan make:controller Api/V1/PostsController --resource
+
+$ php artisan make:migration add_phonenumber_to_customers_table --table=customers
+
+$ mysql -u root -p 'password for root from .env'
+$ GRANT ALL ON laravel.* TO 'laraveluser'@'%' IDENTIFIED BY 'your_laravel_db_password';
+$ docker-compose exec app php artisan make:test UserTest
+$ docker-compose exec app php artisan make:test UserTest --unit
+$ docker-compose exec app php artisan test
+$ docker-compose exec app php artisan migrate:fresh --seed
+$ docker-compose exec app php artisan make:resource V1/InvoiceResource
+
+$ docker-compose exec app php artisan ui vue --auth
+
+$ npm install && npm run dev
+
+
+$ http://localhost/api/v1/customers?postalCode[gt]=30000&type[eq]=I // filter
+
+```
---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,54 @@
+<template>
+      <div id="nav">
+        <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Logo Here</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+      
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <div class="navbar-nav mr-auto">
+
+                  <div class="" v-for="link in links">
+                    <router-link :to="link.href" class="nav-link">{{ link.title }}</router-link>
+                  </div>
+              </div>
+              <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+          </div>
+      </nav>
+       </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        data() {
+            return {
+                links: [
+                    {
+                        title: "Home",
+                        href: "/"
+                    },
+                    {
+                        title: "Blog",
+                        href: "/blog"
+                    },
+                    {
+                        title: "Create post",
+                        href: "/create"
+                    }
+                ]
+            }
+        },
+        mounted() {
+            console.log('Component Test 1255')
+        }
+    }
+</script>
+<style lang="scss">
+    @import '/resources/sass/app.scss';
+</style>
